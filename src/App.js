c2734e6b478c600cde3515eb7fff884a409b9aae
jannie <jannien@utexas.edu>
2025-03-14 22:39:49 -0500

    fixed timezones

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ function App() {
   const [error, setError] = useState('');
   const [cityCoordinates, setCityCoordinates] = useState({});
 
-  const fetchWeatherForecast = async (latitude, longitude) => {
+  const fetchWeatherForecast = async (latitude, longitude, timezone) => {
     try {
       setError('');
 
-      const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&temperature_unit=fahrenheit`);
+      const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&temperature_unit=fahrenheit&timezone=${timezone}`);
       const responseData = await response.json();
       
       const temperatures = responseData.hourly.temperature_2m;
+      const cityUTCOffset = responseData.utc_offset_seconds / 60 / 60;
+      const localUTCOffset = 5;
+      const timezoneOffset = localUTCOffset + cityUTCOffset;
 
-      const currentHour = new Date().getHours();
+      const currentHour = new Date().getHours() + timezoneOffset;
       const forecastData = [];
       for (let i = currentHour; i < currentHour + 12; i++) {
         let ampm;
@@ -30,7 +33,7 @@ function App() {
 
         let forecastHour;
         if (i === 12 || i === 24) {
-          forecastHour = i;
+          forecastHour = 12;
         } else {
           forecastHour = i % 12;
         }
@@ -54,13 +57,13 @@ function App() {
       const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`);
       const geocodingData = await response.json();
 
-      const { latitude, longitude } = geocodingData.results[0];
+      const { latitude, longitude, timezone } = geocodingData.results[0];
       setCityCoordinates(prevState => ({
         ...prevState,
-        [city]: { latitude, longitude }
+        [city]: { latitude, longitude, timezone }
       }));
 
-      return { latitude, longitude };
+      return { latitude, longitude, timezone };
     } catch (err) {
       setError(`Could not find weather for ${city}`);
     }
@@ -68,12 +71,12 @@ function App() {
 
   const handleCityButtonClick = async (city) => {
     if (cityCoordinates[city]) {
-      const { latitude, longitude } = cityCoordinates[city];
-      fetchWeatherForecast(latitude, longitude);
+      const { latitude, longitude, timezone } = cityCoordinates[city];
+      fetchWeatherForecast(latitude, longitude, timezone);
     } else {
       const coordinates = await fetchCityCoordinates(city);
       if (coordinates) {
-        fetchWeatherForecast(coordinates.latitude, coordinates.longitude);
+        fetchWeatherForecast(coordinates.latitude, coordinates.longitude, coordinates.timezone);
       }
     }
   }
@@ -86,7 +89,7 @@ function App() {
       if (coordinates) {
         setCities([...cities, cityInput]);
         setCityInput('');
-        fetchWeatherForecast(coordinates.latitude, coordinates.longitude);
+        fetchWeatherForecast(coordinates.latitude, coordinates.longitude, coordinates.timezone);
       }
     } else {
       setError("City is already added!");
@@ -97,7 +100,7 @@ function App() {
     const loadInitialData = async () => {
       const coordinates = await fetchCityCoordinates('Austin');
       if (coordinates) {
-        fetchWeatherForecast(coordinates.latitude, coordinates.longitude);
+        fetchWeatherForecast(coordinates.latitude, coordinates.longitude, coordinates.timezone);
       }
     };
     loadInitialData();
